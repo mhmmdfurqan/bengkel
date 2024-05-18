1d608e34f4ba137dea030fe9b7c7dcb62f5e4e6d
mhmmdfurqan <mhd.furqan76@gmail.com>
2024-05-19 02:19:42 +0700

    penyesuain index customer (selesai)

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -26,36 +26,20 @@
                                     </thead>
                                    
                                     <tbody>
+                                        @forelse ($cus as $item)
                                         <tr>
-                                            <td>1</td>
-                                            <td>Ucung D</td>
-                                            <td>Surabaya</td>
-                                            <td>111111111111</td>
+                                        <td>{{$nomor++}}</td>
+                                        <td>{{$item->nama_customer}}</td>
+                                        <td>{{$item->alamat_customer}}</td>
+                                        <td>{{$item->telp_customer}}</td>  
                                             <td>
                                                 <a href="#" class="btn btn-info btn-sm"><i class="fa fa-pencil-alt"></i></a>
                                                 <a href="#" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></a>
                                             </td>
-                                        </tr>
-                                        <tr>
-                                            <td>2</td>
-                                            <td>Garrett Winters</td>
-                                            <td>Surabaya</td>
-                                            <td>222222222222</td>
-                                            <td>
-                                                <a href="#" class="btn btn-info btn-sm"><i class="fa fa-pencil-alt"></i></a>
-                                                <a href="#" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></a>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td>3</td>
-                                            <td>Ashton Cox</td>                                                                                  
-                                            <td>Surabaya</td>
-                                            <td>333333333333</td>
-                                            <td>
-                                                <a href="#" class="btn btn-info btn-sm"><i class="fa fa-pencil-alt"></i></a>
-                                                <a href="#" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></a>
-                                            </td>
-                                        </tr>                                       
+                                        </tr>                                        
+                                        @empty
+
+                                        @endforelse                                       
                                     </tbody>
                                 </table>
                             </div>
