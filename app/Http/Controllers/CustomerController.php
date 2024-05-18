359ff20cfa19c8ef1de3c36e614d8f1eb8840b53
mhmmdfurqan <mhd.furqan76@gmail.com>
2024-05-19 01:16:34 +0700

    membuat form tambah customer part 1

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -19,7 +19,7 @@ class CustomerController extends Controller
      */
     public function create()
     {
-        //
+        return view('customer.form');
     }
 
     /**
--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -3,9 +3,14 @@
 @section('heading','Halaman Customer')
 
 @section('content')
-<div class="card shadow mb-4">
+                <div class="card shadow mb-4">
                         <div class="card-header py-3">
                             <h6 class="m-0 font-weight-bold text-primary">Data Customer</h6>
+                        </div>  
+                        <div class="card-header">
+                        <h3 class="text-left">                           
+                            <a href="/customer/form/" class="btn btn-primary"><i class="fa fa-folder-plus"></i> Tambah Data</a>
+                        </h3>
                         </div>
                         <div class="card-body">
                             <div class="table-responsive">
@@ -15,7 +20,8 @@
                                             <th>No</th>
                                             <th>Nama Customer</th>
                                             <th>Alamat Customer</th>
-                                            <th>Nomor Telepon</th>                                            
+                                            <th>Nomor Telepon</th>  
+                                            <th>Aksi</th>                                          
                                         </tr>
                                     </thead>
                                    
@@ -25,18 +31,30 @@
                                             <td>Ucung D</td>
                                             <td>Surabaya</td>
                                             <td>111111111111</td>
+                                            <td>
+                                                <a href="#" class="btn btn-info btn-sm"><i class="fa fa-pencil-alt"></i></a>
+                                                <a href="#" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></a>
+                                            </td>
                                         </tr>
                                         <tr>
                                             <td>2</td>
                                             <td>Garrett Winters</td>
                                             <td>Surabaya</td>
                                             <td>222222222222</td>
+                                            <td>
+                                                <a href="#" class="btn btn-info btn-sm"><i class="fa fa-pencil-alt"></i></a>
+                                                <a href="#" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></a>
+                                            </td>
                                         </tr>
                                         <tr>
                                             <td>3</td>
                                             <td>Ashton Cox</td>                                                                                  
                                             <td>Surabaya</td>
                                             <td>333333333333</td>
+                                            <td>
+                                                <a href="#" class="btn btn-info btn-sm"><i class="fa fa-pencil-alt"></i></a>
+                                                <a href="#" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></a>
+                                            </td>
                                         </tr>                                       
                                     </tbody>
                                 </table>
